054d6c4f318eb697d49603edcea50178bc1ff151
Hao Hou <ghost89413@gmail.com>
2015-08-17 00:07:48 -0700

    New AST base class

---
+++ src/com/github/_38/radiation/CodeMaker.scala
@@ -0,0 +1,59 @@
+import com.github._38.radiation.ast._
+import scala.language.postfixOps
+import scala.language.implicitConversions
+
+package com.github._38.radiation.CodeMaker {
+    object Conversions {
+        implicit def stringToPattern(s:String) = new PlainText(s)
+        implicit def nodeToPattern(n:Node) = new SignleNode(n)
+        implicit def primitiveToCompound(p:Primitive) = new Compound(List(p))
+        def mkList(l:List[Node], s:String = "") = new NodeList(l, s)
+    }
+  
+    case class CodeInfo(node: Node, text:String, offset:Int){  /* offset against the begning of this pattern */
+        def +(base:Int) = CodeInfo(node, text, offset + base)
+    }
+    
+    
+    trait CodeGeneratePattern {
+        import Conversions._
+        def render:String
+        def info:List[CodeInfo] = List()
+        def length = render length
+        def -- (that: CodeGeneratePattern):Compound = (this,that) match {
+            case (l:Compound,r:Compound) => new Compound((l values) ++ (r values))
+            case (l:Compound,r:Primitive) => new Compound((l values) :+ r)
+            case (l:Primitive, r) => (l:Compound) -- r
+        }
+    }
+    
+    trait Primitive extends CodeGeneratePattern;
+    
+    trait ImportantPrimitive extends CodeGeneratePattern with Primitive;
+    
+    class PlainText(text:String) extends CodeGeneratePattern with Primitive {
+        def render = text;
+    }
+    class NodeList(nodes:List[Node], seperator:String) extends CodeGeneratePattern with ImportantPrimitive {
+        def render = nodes map (_ targetCode) mkString seperator
+        def _scan(todo:List[Node], offset:Int):List[CodeInfo] = todo match {
+            case x :: xs => CodeInfo(x, x targetCode, offset) :: _scan(xs, (x length) + offset + (seperator length))
+            case _ => List()
+        }
+        override def info = _scan(nodes, 0)
+    }
+    class SignleNode(node:Node) extends CodeGeneratePattern with ImportantPrimitive {
+        def render = node targetCode
+        override def info = List(CodeInfo(node, node targetCode, 0))
+    }
+    class Compound(val values:List[Primitive]) extends CodeGeneratePattern {
+        def render = values map (_ render) mkString
+        def _scan(xs:List[Primitive], base:Int):List[CodeInfo] = xs match {
+            case x :: xs => x.info.map(_ + base) ++ _scan(xs, base + x.length)
+            case _ => List()
+        }
+        override def info = _scan(values, 0)
+    }
+}
+
+// vim: set ts=4 sw=4 et:
--- src/com/github/_38/radiation/ast.scala
+++ src/com/github/_38/radiation/ast.scala
@@ -1,43 +1,54 @@
 import org.mozilla.javascript.{ast => RhinoAST, Parser}
+import com.github._38.radiation.CodeMaker.{Conversions, CodeInfo, CodeGeneratePattern}
 import scala.math.max
 import scala.language.postfixOps
 import scala.language.implicitConversions
 
 package com.github._38.radiation.ast {
+    import Conversions._
     /** Describe a Location in a source code */
     case class SourceLocation(val line:Int, val column:Int);
     /** The base class for all AST Nodes */
     abstract class Node(location:SourceLocation) {
-        def targetCode:String;
-        val length:Int;
-        val sourceLocation:SourceLocation = location;
+        val pattern:CodeGeneratePattern;
+        lazy val length:Int = pattern length; 
+        def targetCode:String = pattern render;
+        def targetCodeInfo:List[CodeInfo] = pattern info
+        def sourceLocation:SourceLocation = location
     }
     object Node {
-        def mkList(fmt:String, sep:String, values:List[Node]) =
-            fmt format (values map (_ targetCode) mkString sep)
-        def listLength(wrap:Int, sep:Int, values:List[Node]) = 
-            wrap + (values map (_ length) sum) + (values length) * sep - sep
+        def main(s:Array[String]) {
+            val l = SourceLocation(0,0)
+            System.out.println(Lst(List(Num("1", l), Num("2", l)), l).targetCodeInfo)
+        }
     }
+    
     trait Statement extends Node;
     trait Expression extends Statement;
     trait Scope extends Node;
-    
-    case class Num(value:String, location:SourceLocation) extends Node(location) with Expression {
-        def targetCode = value
-        lazy val length = value.length
-    }
-    case class Lst(children:List[Node], location:SourceLocation) extends Node(location) with Expression {
-        def targetCode = Node.mkList("[%s]", ",", children)
-        lazy val length = Node.listLength(2, 1, children)
-    }
+    trait ControlFlow extends Statement;
+   
     case class Block(statements:List[Statement], location:SourceLocation) extends Node(location) with Statement {
-        def targetCode = Node.mkList("{%s}", "", statements)
-        lazy val length = Node.listLength(2, 0, statements)
+        val pattern = "{" -- mkList(statements) -- "}"
     }
-    case class Catch(val cond:Node, val body:Block) extends Node(location) {
-        def targetCode = "try(%s)%s".format(cond targetCode, body targetCode)
-        lazy val length = 5 + (cond length) + (body length)
+    case class Break(location:SourceLocation) extends Node(location) with ControlFlow {
+        val pattern = "break;":CodeGeneratePattern
     }
-    
+    case class Catch(cond:Node, body:Block, location:SourceLocation) extends Node(location) {
+        val pattern = if(cond != null) "catch(" -- cond -- ")"  -- body else "finally" -- body
+    }
+    case class ?:(cond:Expression,  trueExpr:Expression, falseExpr:Expression, location:SourceLocation) extends Node(location) with Expression {
+        val pattern = cond -- "?" -- trueExpr -- ":" -- falseExpr
+    }
+    case class Lst(values:List[Node], location:SourceLocation) extends Node(location) with Expression {
+        val pattern = "[" -- mkList(values, ",") -- "]"
+    }
+    case class Num(value:String, location:SourceLocation) extends Node(location) with Expression {
+        val pattern = value:CodeGeneratePattern;
+    }
+    case class Try(val tryBlock:Block, val catchBlocks:List[Catch], location:SourceLocation) extends Node(location) with Statement {
+        val pattern = "try" -- tryBlock -- mkList(catchBlocks)
+    }
+   
 }
 
