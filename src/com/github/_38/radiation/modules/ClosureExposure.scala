5371fb5e7ae198b1ae99a2f85a7034e4b8ffc935
Hao Hou <ghost89413@gmail.com>
2015-08-20 18:07:16 -0700

    Change identation

--- src/com/github/_38/radiation/modules/ClosureExposure.scala
+++ src/com/github/_38/radiation/modules/ClosureExposure.scala
@@ -5,34 +5,34 @@ package com.github._38.radiation.modules.closure {
 	object ClosureExposure {
 		import Template._
 		import ast.{Node, End, Patch, Program, Nop, Bundle, FuncDef, Num, Id}
-		val funcDef = """$$0$$;  /* The function declearation */
-		                 $$1$$ = _$_$($$2$$,  /* The index */
-		                              $$1$$,  /* Put the name here */
-		                              $$3$$   /* The closure object */
-		                 );""".t
-		val funcExp = """_$_$($$0$$ /* The index */,
-		                     $$1$$ /* The function */ ,
-		                     $$2$$ /* the Closure object */)""".e
-		val header  = """try
-		                {
-		                   __$_closure_dict__;
-	                    }
-		                catch(e)
-		                {
-		                   __$_closure_dict__ = {};
-	                    }
-		                function _$_$(id, func, closure)
-		                {
-		                   func.__closure__  = closure;
-		                   if(__$_closure_dict__[id] === undefined)
-		                   {
-		                       __$_closure_dict__[id] = [];
-	                       }
-		                   func.__closure__.id = __$_closure_dict__[id].length;
-		                   func.__closure__.func = id;
-		                   __$_closure_dict__[id].push(func);
-		                   return func;
-	                    }""".js
+        val funcDef = """$$0$$;  /* The function declearation */
+                         $$1$$ = _$_$($$2$$,  /* The index */
+                                      $$1$$,  /* Put the name here */
+                                      $$3$$   /* The closure object */
+                         );""".t
+        val funcExp = """_$_$($$0$$ /* The index */,
+                             $$1$$ /* The function */ ,
+                             $$2$$ /* the Closure object */)""".e
+        val header  = """try
+                        {
+                           __$_closure_dict__;
+                        }
+                        catch(e)
+                        {
+                           __$_closure_dict__ = {};
+                        }
+                        function _$_$(id, func, closure)
+                        {
+                           func.__closure__  = closure;
+                           if(__$_closure_dict__[id] === undefined)
+                           {
+                               __$_closure_dict__[id] = [];
+                           }
+                           func.__closure__.id = __$_closure_dict__[id].length;
+                           func.__closure__.func = id;
+                           __$_closure_dict__[id].push(func);
+                           return func;
+                        }""".js
 		
 		def visitor(node:Node):Node = node match {
 			case End(Program(_)) => Patch(0, header, 0)   /* Add the header */
