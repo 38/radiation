798c3e9e3d2ff7da087ce10b9a4fc6c1b481d95c
Hao Hou <ghost89413@gmail.com>
2015-09-09 22:50:10 -0700

    almost deleted everything

--- src/com/github/_38/radiation/ast/AST.scala
+++ src/com/github/_38/radiation/ast/AST.scala
@@ -2,27 +2,6 @@ package com.github._38.radiation.ast
 
 import org.mozilla.javascript.ast._
 import com.github._38.radiation.source.{Location}
-/** The notation for identifying the node type */
-trait NodeType;
-object NodeType {
-    /* How node data stored */
-    /** Fixed Length Node means the node has fixed number of child */
-    trait F extends NodeType;
-    /** Variable Length Node */
-    trait V extends NodeType;
-
-    /* Define the node type group */
-    /** The group of node types that represents Expression */
-    trait Expression;
-    /** The group of node types that represent a Statement */
-    trait Statement;
-
-    object Lexical extends F;
-    object Block extends F with Statement;
-    object Break extends F with Statement;
-    object StatementList extends V;
-
-}
 /** The unified node type
  *  @note this means we do not have subclass of node anymore. 
  *        The design decision was made because we need to modify the 
@@ -30,10 +9,6 @@ object NodeType {
  *        If we have different inherated class for different node,
  *        It's difficult to change it
  */
-abstract class Node(val type:NodeType);
+class Node(val type:NodeType) {
 
-/** The node that is the combination of other nodes */
-class Combination(type:NodeType, val children: List[Node]) extends Node(type);
-
-/** The node that represents a lexical token */
-class Lexical(val what:String, val where:Location) extends Node(NodeType.Lexical) ;
+}
