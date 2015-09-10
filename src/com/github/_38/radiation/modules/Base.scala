895b1955ad60b42ab95bdedfa49e1368bc275cf6
Hao Hou <ghost89413@gmail.com>
2015-09-09 17:52:59 -0700

    new Ast

--- src/com/github/_38/radiation/modules/Base.scala
+++
@@ -1,6 +0,0 @@
-import com.github._38.radiation.ast.Node
-package com.github._38.radiation.modules {
-	trait ASTModule {
-		def run(ast:Node):Node
-	}
-}
---
+++ src/com/github/_38/radiation/source/Location.scala
@@ -0,0 +1,10 @@
+package com.github._38.source;
+/** Describe the location in the source code */
+trait Location;
+/** The location of the source code is not defined */
+case object Undefined extends Location;
+/** Case describe &lt;File, Line, Column&gt; */
+case class SourceLocation(file:String, line:Int, column:Int) {
+    override toString() = "%s@%d:%d".format(file, line, column)
+}
+
