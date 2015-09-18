d9d8f0acdf3fa9114fdbe38d7c697c7e2837cb7d
Hao Hou <ghost89413@gmail.com>
2015-09-18 13:25:24 -0700

    Added IE style symbol list

--- src/com/github/_38/radiation/sourcemap/SourceMap.scala
+++ src/com/github/_38/radiation/sourcemap/SourceMap.scala
@@ -4,25 +4,24 @@ import scala.annotation.tailrec
 import scala.collection.mutable.ListBuffer
 
 import com.github._38.radiation.source.InSource
-import com.github._38.radiation.ast.{Node, Complex, Lexical, Nothing}
+import com.github._38.radiation.ast.{Node, Complex, Lexical, Nothing, FuncExpr, Id, Block}
 import com.github._38.radiation.ast.Helper.whiteSpaces
 
 import org.mozilla.javascript.ScriptRuntime.escapeString
 
 case class SourceMapItem(val source:InSource, val target:InSource, val symbol:Option[String])
-// TODO: because symbol sometimes gone, so we need ? 
 
 class SourceMap(val mappings:List[SourceMapItem]) {
 	import Base64Int._
 	import Base64IntConverters._
 	val version = 3
-	private class _SourceMapInteral(val sourceFile:Int,
+	private class _SourceMapInternal(val sourceFile:Int,
 	                                val symbol:Int,
 	                                val sourceLine:Int,
 	                                val sourceColumn:Int,
 	                                val targetLine:Int,
-	                                val targetColumn:Int) extends Ordered[_SourceMapInteral]{
-		def compare(that:_SourceMapInteral) =
+	                                val targetColumn:Int) extends Ordered[_SourceMapInternal] {
+		def compare(that:_SourceMapInternal) =
 		    if      (targetLine   != that.targetLine)   targetLine - that.targetLine
 		    else if (targetColumn != that.targetColumn) targetColumn - that.targetColumn
 		    else if (sourceFile   != that.sourceFile)   sourceFile - that.sourceFile
@@ -30,7 +29,7 @@ class SourceMap(val mappings:List[SourceMapItem]) {
 		    else if (sourceColumn != that.sourceColumn) sourceColumn - that.sourceColumn
 		    else if (symbol       != that.symbol)       symbol - that.symbol
 		    else 0
-		def -(that:_SourceMapInteral):List[Int] = {
+		def -(that:_SourceMapInternal):List[Int] = {
 			val dTargetColumn = targetColumn - that.targetColumn
 			val dSourceFile   = sourceFile - that.sourceFile
 			val dSourceLine   = sourceLine - that.sourceLine
@@ -40,13 +39,15 @@ class SourceMap(val mappings:List[SourceMapItem]) {
 			if(symbol == -1) return dTargetColumn :: dSourceFile :: dSourceLine :: dSourceColumn :: Nil
 			dTargetColumn:: dSourceFile :: dSourceLine :: dSourceColumn :: dSymbol :: Nil
 		}
-		def newLine(n:Int) = new _SourceMapInteral(sourceFile, symbol, sourceLine, sourceColumn, targetLine + n, 0)
+		def newLine(n:Int) = new _SourceMapInternal(sourceFile, symbol, sourceLine, sourceColumn, targetLine + n, 0)
 		override def toString = "(%d,%d,%d,%d,%d,%d)".format(sourceFile, symbol, sourceLine, sourceColumn, targetLine, targetColumn)
-		//TODO keep symbol id ....
+		def getLast(that:_SourceMapInternal) = 
+			if(symbol == -1) new _SourceMapInternal(sourceFile, that.symbol, sourceLine, sourceColumn, targetLine, targetColumn)
+			else this
 	}
 	@tailrec
 	private def _toInternal(mappings:List[SourceMapItem], 
-							result:Array[_SourceMapInteral], 
+							result:Array[_SourceMapInternal], 
 							sources:Map[String, Int], 
 							symbols:Map[String, Int],
 							n:Int):(Map[String, Int], Map[String, Int]) =
@@ -55,7 +56,7 @@ class SourceMap(val mappings:List[SourceMapItem]) {
 			    val sourceFile = item.source.file
 			    val newSources = if(sources contains sourceFile) sources else sources + (sourceFile -> sources.size)
 				val newSymbols = item.symbol match {
-					case Some(symbol) if (!(symbols contains symbol)) => symbols + symbol
+					case Some(symbol) if (!(symbols contains symbol)) => symbols + (symbol -> symbols.size)
 					case _                                            => symbols
 				}
 			    val fileIdx = newSources(sourceFile)
@@ -63,8 +64,8 @@ class SourceMap(val mappings:List[SourceMapItem]) {
 					case Some(symbol) => newSymbols(symbol)
 					case None         => -1
 				}
-			    result(n) = new _SourceMapInteral(fileIdx, symbolIdx, item.source.line, item.source.column, item.target.line, item.target.column)
-			    _toInternal(xs, result, newSources, n + 1)
+			    result(n) = new _SourceMapInternal(fileIdx, symbolIdx, item.source.line, item.source.column, item.target.line, item.target.column)
+			    _toInternal(xs, result, newSources, newSymbols, n + 1)
 		    }
 		    case Nil       => (sources, symbols)
 	    }
@@ -79,22 +80,24 @@ class SourceMap(val mappings:List[SourceMapItem]) {
 				if(first) result append '['
 				else result append ','
 				result append "\"%s\"".format(escapeString(f, '"'))
+				first = false
 			}
 			result append "]"
 		}
 		
-		val buf = new Array[_SourceMapInteral](mappings.length)
-		val (sources, symbols) = _toInternal(mappings, buf, Map(), 0)
+		val buf = new Array[_SourceMapInternal](mappings.length)
+		val (sources, symbols) = _toInternal(mappings, buf, Map(), Map(), 0)
 		scala.util.Sorting.quickSort(buf)
 		result append "{"
 		result append "\"version\":3,"
 		result append "\"sourceRoot\":\"\","
 		_dumpList("sources", sources.toList.map(x => (x._2, x._1)).sorted.map(x => x._2))
 		result append ","
-		result append "\"names\":[],"
+		_dumpList("names", symbols.toList.map(x => (x._2, x._1)).sorted.map(x => x._2))
+		result append ","
 		result append "\"mappings\":\""
 		var currentLine = 0
-		var lastItem = new _SourceMapInteral(0,-1,0,0,0,0)
+		var lastItem = new _SourceMapInternal(0,-1,0,0,0,0)
 		var first = true
 		for(item <- buf){
 			if(item.targetLine != currentLine) {
@@ -104,7 +107,7 @@ class SourceMap(val mappings:List[SourceMapItem]) {
 			} else if(!first) result append ","
 			else first = false
 			for(num <- (item - lastItem)) result append Base64Int(num).encode
-			lastItem = item
+			lastItem = item.getLast(lastItem)
 		}
 		result append "\""
 		result append "}"
@@ -113,14 +116,14 @@ class SourceMap(val mappings:List[SourceMapItem]) {
 }
 object SourceMap {
 	def fromAST(ast:Node, target:String) = {
-		def _fromAST(ast:Node, target:String, result:ListBuffer[SourceMapItem], scopeName:String = "" ,offset:Int = 0) {
+		def _fromAST(ast:Node, target:String, result:ListBuffer[SourceMapItem], scopeName:String, offset:Int) {
 			ast match {
 				case Lexical(_, where:InSource)  => result += SourceMapItem (where, InSource(target, 0, offset), if(scopeName == "") None else Some(scopeName))
 				case c:Complex => {
 					var ofs = offset
 					var prevous:Node = null
 					val childScope = c match {
-						case FuncExpr(Some(Id(Lexical(what, _))), _)	=> if(scopeName == "") what else scopeName + "." + what
+						case FuncExpr(Some(Id(Lexical(what, _))), _, _) => if(scopeName == "") what else scopeName + "." + what
 						case _                                          => scopeName
 					}
 					for(child <- c.child) {
@@ -135,7 +138,7 @@ object SourceMap {
 			}
 		}
 		val buffer = new ListBuffer[SourceMapItem]()
-		_fromAST(ast, target, buffer, 0)
+		_fromAST(ast, target, buffer, "", 0)
 		new SourceMap(buffer.toList)
 	}
 }
