52a900e8b5a2f50340ec203b8999e85ed8515240
Hao Hou <ghost89413@gmail.com>
2015-09-27 13:54:54 -0700

    TODO: the intepreter that emit the tracked code

--- doc/tracer.js
+++ doc/tracer.js
@@ -1,28 +1,55 @@
-_$_next_tag_$_ = 0
-function _$_pack_$_(what, deps) {
-	console.debug(_$_next_tag_$_ + " << " + deps)
-	return {
-		_$_packed_$_: true,
-		value: what,
-		tag: _$_next_tag_$_ ++
+/* if the global is not initliazed, initialize it */
+try {
+	$t
+} catch (e) {
+	/* The helper object */
+	$t={
+		/* Encode base64 VLQ integer */
+		encode: function () {
+			var termset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdef"
+			var nonterm = "ghijklmnopqrstuvwxyz0123456789+/"
+			var last = 0
+			var absolute = true
+			function raw(value) {
+				var res = ""
+				for(;value >= 32; value >>= 5) res += nonterm[value % 32]
+				res += termset[value]
+				return res
+			}
+			function vlq(value) {
+				if(value < 0) return raw(-value * 2 + 1)
+				else return raw(value * 2)
+			}
+			return function (value) {
+				var d = value - last
+				var dl = vlq(d)
+				var al = raw(value)
+				var ret = ""
+				if(absolute && dl.length + 1 < al.length) {
+					absolute = false
+					ret += "."
+				}
+				if(!absolute && al.length + 1 < dl.length) {
+					absolute = true
+					ret += "."
+				}
+				last = value
+				if(absolute) return ret + al
+				else return ret + dl
+			}
+		}(),
+		writeLog: function(tag, deps) {
+			
+		},
+		nextId: function() {
+			var nextId = 0
+			return function () {
+				return nextId ++
+			}
+		}(),
+		wrap: function(value) {
+			var deps = arguments
+			return deps
+		}
 	}
-}
-function _$_unpacked_$_(what) {
-	return what.value
-}
-function _$_unpack_if_packed_$_(what) {
-	if(what._$_packed_$_ == true) 
-		return what.value
-	return what
-}
-
-/** examples **/
-function fib(n) {
-	if(_$_unpack_if_packed_$_(n) < 1) return _$_pack_$_(1, [])
-	return function () {
-		var x = fib(n - 1)
-		var y = fib(n - 2)
-		var deps = [x.tag, y.tag]
-		return _$_pack_$_(_$_unpack_if_packed_$_(x) + _$_unpack_if_packed_$_(y), deps)
-	}()
 }
