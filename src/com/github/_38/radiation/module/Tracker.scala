56c9e560100a426c05e6f242a5e81efc46b1549e
Hao Hou <ghost89413@gmail.com>
2015-10-01 15:30:09 -0700

    add shortcuts, describe how to write the code transformer

--- src/com/github/_38/radiation/module/Tracker.scala
+++ src/com/github/_38/radiation/module/Tracker.scala
@@ -4,7 +4,6 @@ import scala.language.implicitConversions
 
 import com.github._38.radiation.template.Template._
 import com.github._38.radiation.ast._
-import com.github._38.radiation.module.TrackerJS
 import Helper.fromString
 
 /** @note we should assume that the ast is already processed by closure exposure module
@@ -12,17 +11,28 @@ import Helper.fromString
  *        That means we use __closure__ object to check the boundary of tracking scope
  */
 object Tracker extends ModuleBase {
-	/*
-	private def traverse(root:Node, parentType:NodeType):Unit = root match {
-	    case node:Complex => {
-	        if(node.nodeType.isInstanceOf[Expression] && (!node.finalFlag) && (!parentType.isInstanceOf[Expression])) {
-	            System.out.println(node.nodeType + "\t" + node.finalFlag + "\t" + node.targetCode)
-            }
-	        node.child.foreach(ch => traverse(ch, node.nodeType))
-	        //TODO create new childnode if needed
-        }
-	    case _ => ()
-    }*/
+	private def _modifyLHS(ast:Node, stack:List[Node], localScope:Boolean, readMap:Map[Id, Node], writeMap:Map[Id,Write], expr:Node):(Map[Id,Node], Map[Id,Node], Node)
+	{
+		//TODO transform the left hand-side, basically this will produce a read-set write-set and a transformed expression
+	}
+	private def _modifyExpr(ast:Node, stack:List[Node], localScope:Boolean) = {
+		//TODO transform this expression to a function with read section, computation section and write section
+		// The pattern of this should be
+		// function () {
+		//    $ReadSection$ //read section can be function apply, that means we modify all arguments by this function and then export them
+		//    result = emit($ModifiedExpression$, redSection's tags, code localtion)
+		//    //writeSet = result
+		//    return result
+		// } ()
+	}
+	private def _modifyStmt(ast:Node, stack:List[Node], localScope:Boolean) = {
+		//TODO check if this needs a unpack or export
+		// if, switch and for condiftion, while condition, catch condition needs to be unpacked
+		
+	}
+	private def _modifyProgram(ast:Node, stack:List[Node]) = ast match {
+		case Program(stmts) => stmts.map(stmt => _modifyStm(stmt, stmt :: stack, false))
+	}
 	def run(ast:Node):Node = {
 		val Program(result) = ast
 		new Complex(Program, TrackerJS.header ++ result)
