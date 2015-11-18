47ae6be3edc98c6c72a32d61d7b4f98cf51cc49e
Hao Hou <ghost89413@gmail.com>
2015-11-17 21:49:53 -0800

    why the line # not correct for the large files

--- src/com/github/_38/radiation/Entry.scala
+++ src/com/github/_38/radiation/Entry.scala
@@ -2,41 +2,66 @@ package com.github._38.radiation
 import com.github._38.radiation.ast._
 import com.github._38.radiation.source._
 
-class Result(location:Location);
+import scala.collection.mutable.{Map => MutableMap}
 
 object Main {
-	def analyzeAST(root:Node):(Set[String], Set[Result]) = root match {
+
+	case class Result(symbol:String, location:Location) {
+		override def toString = "symbol `" + symbol + "' has never been used after decleared. (" + location + ")"
+	}
+	case class AnalyzerResult(val refSyms:Set[String], val symsDecl:Map[String, Location], val result:Set[Result]) {
+		def +(that:AnalyzerResult) = AnalyzerResult(refSyms ++ that.refSyms, symsDecl ++ that.symsDecl, result ++ that.result)
+	}
+
+	val EmptyResult = AnalyzerResult(Set(), Map(), Set())
+
+	def analyzeAST(root:Node):AnalyzerResult = root match {
 		case node:Complex => {
-			val (refSyms, result) = (node match {
+			val newSymbols = MutableMap[String, Location]()
+			val AnalyzerResult(refSyms, symbols, result) = (node match {
 				case DeclList((_, varlist)) => varlist.foldLeft(List[Node]())((lst, nxt) => nxt match {
-					case Decl((_, Some(initval))) => initval :: lst
+					case Decl((Id(Lexical(what, where)), Some(initval))) => {
+						newSymbols += (what -> where) 
+						initval :: lst
+					}
 					case _ => lst
 				})
-				case FuncExpr((_, _, body)) =>  List(body)
+				case FuncExpr((name, Arguments(args), body)) =>  {
+					args.foreach((node) => node match {
+						case Id(Lexical(what, where)) => newSymbols += (what -> where)
+					})
+					List(body)
+				}
 				case _ => node.child			
-			}).foldLeft((Set[String](), Set[Result]())) ((res, ch) => {
-				val (ref, ret) = analyzeAST(ch)
-				(res._1 ++ ref, res._2 ++ ret)
-			})
-			val (actualRefs, newResults) = node.nodeType match {
+			}).foldLeft(EmptyResult) ((res, ch) => analyzeAST(ch) + res)
+
+			node.nodeType match {
 				case _:Scope => {
 					val Some(scopeInfo:ScopeMetaData) = node.metaData
-					System.out.println(scopeInfo.locals &~ refSyms);
-					(refSyms &~ scopeInfo.locals, /*(scopeInfo.locals &~ refSyms)*/ Set()) // TODO add more results here
+					val symlist = newSymbols.toMap ++ symbols 
+					val parentSym = node match {
+						case FuncExpr(Some(Id(Lexical(what, where))), _, _) => Map(what -> where)
+						case _ => Map[String, Location]()
+					}
+					val unused = scopeInfo.locals &~ refSyms
+					AnalyzerResult(refSyms &~ scopeInfo.locals, parentSym , result ++ unused.map(what => Result(what, symlist(what)))) 
 				}
 				case Id => {
 					val Id(Lexical(what, _)) = node
-					(refSyms + what, Set())
+					AnalyzerResult(refSyms + what, Map() , result)
 				}
-				case _ => (refSyms, Set())
+				case _ => AnalyzerResult(refSyms, newSymbols.toMap ++ symbols, result)
 			}
-			(actualRefs, result ++ newResults)
 		}
-		case _ => (Set(), Set())
+		case _ => EmptyResult
 	}
-	def main(args:Array[String]) {
-		val ast = AST.parseFromSource("../test.js");
-		System.out.println(ast);
-		analyzeAST(ast);
-	}
+	def main(args:Array[String]) = args.foreach(file => {
+		try {
+			val ast = AST.parseFromSource(file)
+			analyzeAST(ast).result.foreach(System.out.println)
+		} catch {
+			case e:java.io.IOException => System.err.println(e)
+			case whatever:Exception => throw whatever
+		}
+	})
 }
--- src/com/github/_38/radiation/source/Location.scala
+++ src/com/github/_38/radiation/source/Location.scala
@@ -5,7 +5,7 @@ trait Location;
 case object NotInSource extends Location
 /** Case describe &lt;File, Line, Column&gt; */
 case class InSource(file:String, line:Int, column:Int) extends Location{
-	override def toString() = "%s@%d:%d".format(file, line, column)
+	override def toString() = "%s@%d:%d".format(file, line + 1, column + 1)
 	def -(that:InSource) = InSource(file, line - that.line, column - that.column)
 }
 
